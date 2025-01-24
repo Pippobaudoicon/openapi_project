ff2ef10da4d1cce7343515d2c41e3d22c778e2f4
FabioLongoni <104061077+FabioLongoni@users.noreply.github.com>
2025-01-24 11:49:25 +0100

    Refactor assets and update Login page layout: remove unused logo, add new SVG icons, and enhance styling

--- client/src/pages/Login.vue
+++ client/src/pages/Login.vue
@@ -1,5 +1,45 @@
 <template>
-    <div>
-        ciao
-    </div>
+    <main>
+        <div class="flex justify-center basis-0.5 h-screen">
+            <div class="flex flex-col justify-center gap-12">
+                <div class="w-full">
+                    <img src="../media/vicsam-dtextr.png" alt="">
+                </div>
+                <div class="px-0 md:px-12">
+                    <form id="loginForm" class="space-y-6 flex flex-col" action="#" method="POST">
+                        <div class="flex items-end gap-5 border-b-2 px-2 py-1 border-gray-300">
+                            <div>
+                                <img class="icon" src="../svg/teacher.svg" alt="">
+                            </div>
+                            <div>
+                                <input id="user" name="user" type="text" autocomplete="user" required class="" placeholder="User">
+                            </div>
+                        </div>
+                        <div class="flex items-end gap-5 border-b-2 px-2 py-1 border-gray-300">
+                            <div>
+                                <img class="icon" src="../svg/secure.svg" alt="">
+                            </div>
+                            <div>
+                                <input id="user" name="user" type="text" autocomplete="user" required class="" placeholder="Password">
+                            </div>
+                        </div>
+                        <div class="w-full text-center bg-blue-400 text-white py-2 cursor-pointer">
+                            <button>ACCEDI ORA</button>
+                        </div>
+			        </form>
+                </div>
+            </div>
+        </div>
+    </main>
 </template>
+
+<style scoped>
+    main {
+        background-image: url('../media/bgapp.jpg');
+        background-size: cover;
+        .icon {
+            width: 35px;
+            height: auto;
+        }
+    }
+</style>

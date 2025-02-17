7c9ede1a1255561a15861fd2b7df42efa2a13550
FabioLongoni <104061077+FabioLongoni@users.noreply.github.com>
2025-02-17 17:23:22 +0100

    Refactor SelectBusinessReport component to implement business report fetching on component mount and enhance template structure

--- client/src/components/business_report/SelectBusinessReport.vue
+++ client/src/components/business_report/SelectBusinessReport.vue
@@ -1,36 +1,10 @@
-<template>
-    <div class="gray-bkg h-full text-sm">
-        <div class=" flex flex-col items-center justify-center h-full">
-            <!-- <div>
-                <div class="text-7xl italic-font light-blue-txt mb-6">
-                    Ricerca Ora
-                </div>
-                <div class="border-b-2 px-2 py-1 border-gray-500">
-                    <input id="vat" name="vat" type="text" autocomplete="vat" required class="w-full focus:outline-none placeholder-black font-bold" placeholder="Partita IVA">
-                </div>
-                <div class="flex justify-between py-5">
-                    <ul>
-                        <li class="flex items-center" v-for="(option, index) in options" :key="index">
-                            <input 
-                                type="checkbox" 
-                                :id="'option-' + index" 
-                                :value="option.value" 
-                                :checked="selectedOption === option.value"
-                                @change="selectOption(option.value)"
-                                class="cursor-pointer">
-                            <span class="ml-2 font-bold capitalize">{{ option.label }}</span>
-                        </li>
-                    </ul>
-                </div>
-                <button class="w-full text-center light-blue-bkg text-white py-2 cursor-pointer uppercase main-font">
-                    avvia ricerca
-                </button>
-            </div> -->
-        </div>
-    </div>
-</template>
-
 <script setup>
+import { ref, onMounted } from 'vue';
+
+const data = ref(null);
+const baseUrl = 'http://localhost:3000/api/v1/';
+const endUrl = '/allegati';
+
 const businessReport = async () => {
     try {
         const response = await fetch('http://localhost:3000/api/v1/visure', {
@@ -40,13 +14,64 @@ const businessReport = async () => {
             },
             credentials: 'include',
         });
-        const data = await response.json();
-        console.log(data);
+        const result = await response.json();
+        data.value = result.data;
     } catch (error) {
-        alert( error.message);
+        alert(error.message);
     }
 };
-window.onload = () => {
-    businessReport();
+
+const formatDate = (timestamp) => {
+    const date = new Date(timestamp * 1000); // Converti in millisecondi
+    return date.toLocaleDateString('it-IT', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+    });
 };
+
+onMounted(() => {
+    businessReport();
+});
 </script>
+
+<template>
+    <div class="gray-bkg h-full text-sm">
+        <div class="p-12 h-custom overflow-auto">
+            <div class="flex flex-col flex-wrap gap-1.5 md:flex-row">
+                <div v-for="item in data" class="bg-white px-4 md:px-12 py-8">
+                    <div class="mb-2 font-semibold text-md md:text-lg">
+                        Partita IVA - {{ item.data.cf_piva_id }}
+                    </div>
+                    <div class="text-md mb-1 font-semibold">
+                        Visura - {{ item.data.tipo }}
+                    </div>
+                    <div class="text-md mb-1 font-semibold">
+                        Ultima estrazione - {{ formatDate(item.data.timestamp_creation) }}
+                    </div>
+                    <div class="text-md mb-1 font-semibold text-gray-400">
+                        Status - {{ item.status }}
+                    </div>
+                    <a :href="`${baseUrl}${item.data.tipo}/${item.requestId}${endUrl}`">
+                        <button 
+                            :class="{ 'opacity-50 cursor-not-allowed': item.status === 'pending', 'cursor-pointer': item.status !== 'pending' }"
+                            :disabled="item.status === 'pending'"
+                            class="w-full text-center light-blue-bkg text-white py-2 uppercase main-font" 
+                        >
+                            scarica allegato
+                        </button>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.h-custom {
+    height: 65vh;
+}
+</style>
